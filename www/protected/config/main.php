4faee623e007c1906f5e8967631a348916cb8b99
Derrick Hammer <derrick@derrickhammer.com>
2013-12-31 18:46:19 -0500

    *Update application name *Removed un-neeed commented code

--- www/protected/config/main.php
+++ www/protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Feathur',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -19,15 +19,6 @@ return array(
 	),
 
 	'modules'=>array(
-		// uncomment the following to enable the Gii tool
-		/*
-		'gii'=>array(
-			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
-			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
-		),
-		*/
 	),
 
 	// application components
