8b37cb715f1cf799ced645e41bb5198ee765a643
Derrick Hammer <derrick@derrickhammer.com>
2013-12-30 21:29:24 -0500

    *Enabled Gii in Yii test config

--- www/protected/config/test.php
+++ www/protected/config/test.php
@@ -3,15 +3,18 @@
 return CMap::mergeArray(
 	require(dirname(__FILE__).'/main.php'),
 	array(
+        'modules' => array(
+            'gii'=>array(
+                'class'=>'system.gii.GiiModule',
+                'password'=>'Enter Your Password Here',
+                // If removed, Gii defaults to localhost only. Edit carefully to taste.
+                'ipFilters'=>array('127.0.0.1','::1'),
+            ),
+        ),
 		'components'=>array(
 			'fixture'=>array(
 				'class'=>'system.test.CDbFixtureManager',
 			),
-			/* uncomment the following to provide test database connection
-			'db'=>array(
-				'connectionString'=>'DSN for test database',
-			),
-			*/
 		),
 	)
 );
